bc1a9f6ca5e36189a742ceb36a587ddd22639e02
antmusumba <musumbaantony3@gmail.com>
2024-06-11 09:31:45 +0300

    BREAKING CHANGE: delet sub.go file

--- main.go
+++ main.go
@@ -7,54 +7,51 @@ import (
 )
 
 func main() {
-	// Provide correct arguments
+	// Check if the correct number of arguments is provided
 	if len(os.Args[1:]) < 2 || len(os.Args[1:]) > 3 {
 		fmt.Println("Not enough arguments")
 		return
 	}
 
+	// If the second argument ends with ".txt", create a new file with the provided content
 	if strings.HasSuffix(os.Args[2], ".txt") {
-			err := os.WriteFile(os.Args[2],[]byte(os.Args[1]),0o644)
-			if err != nil {
+		err := os.WriteFile(os.Args[2], []byte(os.Args[1]), 0o644)
+		if err != nil {
 			fmt.Printf("Error writing to file: %v\n", err)
 			return
 		}
-
 		fmt.Printf("File %s has been generated.\n", os.Args[2])
 	} else {
+		// Handle other operations (e.g., --upper, --cap, --low)
 		if os.Args[2] == "--upper" {
-			n ,_ := os.ReadFile(os.Args[1])
-			n2 := string(n) 
-
+			n, _ := os.ReadFile(os.Args[1])
+			n2 := string(n)
 			fileContent := strings.ToUpper(n2)
 			if err := os.WriteFile(os.Args[1], []byte(fileContent), 0644); err != nil {
 				fmt.Printf("Error writing to file: %v\n", err)
 				return
 			}
 			fmt.Printf("File %s has been modified to uppercase.\n", os.Args[1])
-		} else if  os.Args[2] == "--cap" {
-			n ,_ := os.ReadFile(os.Args[1])
-			n2 := string(n) 
-
+		} else if os.Args[2] == "--cap" {
+			n, _ := os.ReadFile(os.Args[1])
+			n2 := string(n)
 			fileContent := strings.Title(n2)
 			if err := os.WriteFile(os.Args[1], []byte(fileContent), 0644); err != nil {
 				fmt.Printf("Error writing to file: %v\n", err)
 				return
 			}
-			fmt.Printf("File %s has been modified to uppercase.\n", os.Args[1])
+			fmt.Printf("File %s has been modified to title case.\n", os.Args[1])
 		} else if os.Args[2] == "--low" {
-			n ,_ := os.ReadFile(os.Args[1])
-			n2 := string(n) 
-
+			n, _ := os.ReadFile(os.Args[1])
+			n2 := string(n)
 			fileContent := strings.ToLower(n2)
 			if err := os.WriteFile(os.Args[1], []byte(fileContent), 0644); err != nil {
 				fmt.Printf("Error writing to file: %v\n", err)
 				return
 			}
-			fmt.Printf("File %s has been modified to uppercase.\n", os.Args[1])
+			fmt.Printf("File %s has been modified to lowercase.\n", os.Args[1])
 		} else {
 			fmt.Println("Invalid operation.")
 		}
 	}
 }
-
